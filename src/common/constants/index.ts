85ea2a760d8b17b7909cf0208933d634d80dfc8e
Chisom Promise <66853110+Chimise@users.noreply.github.com>
2023-10-19 22:16:20 +0100

    Create a mongoose schema for user (#52)
    * feat: create mongoose schema for user
    
    * feat: create mongoose schema for user
    
    * chore: move @types/bycrypt to dev dependency
    
    * fix: keep schema types consistent and add new fields

---
+++ src/common/constants/index.ts
@@ -0,0 +1,23 @@
+export enum Role {
+  SuperUser = 'superuser',
+  User = 'user',
+  Guest = 'guest',
+}
+
+export enum Provider {
+  Local = 'local',
+  Google = 'google',
+}
+
+export enum IDType {
+  NIN = 'nin',
+  BVN = 'bvn',
+  IntlPassport = 'intl-passport',
+}
+
+export enum Gender {
+  Male = 'male',
+  Female = 'female',
+  Others = 'others',
+  None = 'none',
+}
