866e0e3b96e63447a5421e240d80c0c475e3c566
Klin-Coders <obcbeats@gmail.com>
2023-10-31 23:45:48 +0300

    force

--- src/controllers/resetPasswordController.ts
+++ src/controllers/resetPasswordController.ts
@@ -1,15 +1,15 @@
-import UserModel from '../models/userModel';
 import { NextFunction, Request, Response } from 'express';
-import AppError from '../common/utils/appError';
-import TokenModel from '../models/tokenModel';
+import { catchAsync } from 'src/middlewares';
 import { generateRandomString, hashData } from '../common/utils';
+import AppError from '../common/utils/appError';
 import { AppResponse } from '../common/utils/appResponse';
+import TokenModel from '../models/tokenModel';
+import UserModel from '../models/userModel';
 
-export const resetPassword = async (req: Request, res: Response, next: NextFunction) => {
+export const resetPassword = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 	const { email } = req.body;
 
 	const user = await UserModel.findOne({ email });
-
 	if (!user) {
 		return next(new AppError('no user found with provided email', 404));
 	}
@@ -21,4 +21,4 @@ export const resetPassword = async (req: Request, res: Response, next: NextFunct
 	await TokenModel.create({ user: user._id, token, createdAt: new Date() });
 
 	return AppResponse(res, 200, null, 'Password reset link sent to your email');
-};
+});
--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -4,7 +4,7 @@ import { catchAsync } from 'src/middlewares';
 import { addEmailToQueue } from 'src/queues/emailQueue';
 
 export const test = catchAsync(async (req: Request, res: Response) => {
-	if (!req.body) throw new AppError('Test error', 400);
+	if (req.body) throw new AppError('Test error', 400);
 
 	addEmailToQueue({
 		type: 'passwordResetSuccessful',
