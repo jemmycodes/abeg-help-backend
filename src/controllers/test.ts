92143c852ca0905b1b3aec36c3868a44b8e12324
Klin-Coders <obcbeats@gmail.com>
2023-10-31 21:05:23 +0300

    refactor

--- src/controllers/test.ts
+++ src/controllers/test.ts
@@ -1,22 +1,20 @@
-import { NextFunction, Request, Response } from 'express';
+import { Request, Response } from 'express';
 import AppError from 'src/common/utils/appError';
 import { catchAsync } from 'src/middlewares';
 import { addEmailToQueue } from 'src/queues/emailQueue';
 
-export const test = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-	if (req.body) {
-		return next(new AppError('Test error', 400));
-	} else {
-		addEmailToQueue({
-			type: 'passwordResetSuccessful',
-			data: {
-				to: 'obcbeats@gmail.com',
-				priority: 'high',
-			},
-		});
-		return res.status(200).json({
-			status: 'success',
-			message: 'Test route',
-		});
-	}
+export const test = catchAsync(async (req: Request, res: Response) => {
+	if (!req.body) throw new AppError('Test error', 400);
+
+	addEmailToQueue({
+		type: 'passwordResetSuccessful',
+		data: {
+			to: 'obcbeats@gmail.com',
+			priority: 'high',
+		},
+	});
+	return res.status(200).json({
+		status: 'success',
+		message: 'Test route',
+	});
 });
