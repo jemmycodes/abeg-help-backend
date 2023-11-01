3667237ff8d930c90e69d8f5b9614353e8eac18d
Klin-Coders <obcbeats@gmail.com>
2023-11-01 04:15:13 +0300

    remove redundant export

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,4 +1,3 @@
-export { CatchAsync } from './catchAsyncErrors';
 export { timeoutMiddleware } from './timeout';
 export { validateDataWithZod } from './validateDataWithZod';
 export { wrapRouter } from './wrapRouter';
