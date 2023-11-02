c1caf03cbc4960b89475510890a6348cabb7b2d9
Klin-Coders <obcbeats@gmail.com>
2023-11-02 15:13:04 +0300

    upgrade mongoose

--- src/common/config/database.ts
+++ src/common/config/database.ts
@@ -4,14 +4,13 @@ import { ENVIRONMENT } from './environment';
 import { ConnectOptions } from 'mongoose';
 
 interface CustomConnectOptions extends ConnectOptions {
-	useNewUrlParser: boolean;
+	maxPoolSize?: number;
+	minPoolSize?: number;
 }
 
 export const connectDb = async (): Promise<void> => {
 	try {
 		const conn = await mongoose.connect(ENVIRONMENT.DB.URL, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
 			// minPoolSize: 100,
 			// maxPoolSize: 100,
 		} as CustomConnectOptions);
