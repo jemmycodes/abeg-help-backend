8e537c8ae292f57968da06c5c5c309e0e084d952
Otebele Jemimah (Chef Meemah) <110843645+jemmycodes@users.noreply.github.com>
2024-01-06 22:38:07 +0000

    feat:added the otp verification template

---
+++ src/queues/templates/OtpVerification.ts
@@ -0,0 +1,44 @@
+export const otpVerification = () => {
+    return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="max-width:28rem;width:100%;display:flex;flex-direction:column;justify-content:center;align-items:center;padding:1rem;color:rgb(72,72,72);gap:1.25rem">
+      <tbody>
+        <tr style="width:100%">
+          <td>
+            <style>
+              @font-face {
+                font-family: 'Manrope';
+                font-style: normal;
+                font-weight: 400;
+                mso-font-alt: 'sans-serif';
+                src: url(https://fonts.gstatic.com/s/manrope/v15/xn7gYHE41ni1AdIRggexSg.woff2) format('woff2');
+              }
+    
+              * {
+                font-family: 'Manrope', sans-serif;
+              }
+            </style>
+            <div class="" style="width:fit-content;display:flex;justify-content:center;align-items:center;margin-left:auto;margin-right:auto;gap:0.25rem"><img alt="Logo" height="35" src="/src/assets/Logo.png" style="display:block;outline:none;border:none;text-decoration:none" width="35" />
+              <p style="font-size:1.125rem;line-height:1.75rem;margin:16px 0;font-weight:700;color:rgb(43,144,142)">AbegHelp.me</p>
+            </div>
+            <h1 class="" style="text-align:center;font-size:1.25rem;line-height:1.75rem;font-weight:600">Verification Code </h1><img alt="Illustration" src="/src/assets/illustration.png" style="display:block;outline:none;border:none;text-decoration:none;width:100%" />
+            <p style="font-size:1.125rem;line-height:1.75rem;margin:16px 0;text-align:center">Hi, Locs Designer</p>
+            <p style="font-size:1rem;line-height:1.5rem;margin:16px 0;text-align:center">To login your account, use the verification code below</p>
+            <table align="center" width="100%" class="" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="padding-left:4rem;padding-right:4rem;padding-top:0.5rem;padding-bottom:0.5rem;letter-spacing:0.1em;color:rgb(43,144,142);outline-color:#2B908E;outline-style:dashed;width:fit-content;margin-top:2.5rem;margin-bottom:2.5rem">
+              <tbody>
+                <tr>
+                  <td>771020</td>
+                </tr>
+              </tbody>
+            </table>
+            <p style="font-size:1.125rem;line-height:1.75rem;margin:16px 0;text-align:center">This expires in 30 minutes</p>
+            <p style="font-size:1rem;line-height:1.5rem;margin:16px 0;text-align:center">If you did not initiate this action, kindly disregard this email</p>
+            <div style="width:fit-content;display:flex;justify-content:center;align-items:center;margin-left:auto;margin-right:auto;gap:1rem"><img alt="Logo" height="20" src="/src/assets/facebook.png" style="display:block;outline:none;border:none;text-decoration:none" width="20" /><img alt="Logo" height="20" src="/src/assets/twitter.png" style="display:block;outline:none;border:none;text-decoration:none" width="20" /><img alt="Logo" height="20" src="/src/assets/instagram.png" style="display:block;outline:none;border:none;text-decoration:none" width="20" /><img alt="Logo" height="20" src="/src/assets/linkedin.png" style="display:block;outline:none;border:none;text-decoration:none" width="20" /></div>
+            <div class="" style="width:fit-content;display:flex;justify-content:center;align-items:center;margin-left:auto;margin-right:auto;gap:0.25rem"><img alt="Logo" height="20" src="/src/assets/Logo.png" style="display:block;outline:none;border:none;text-decoration:none" width="20" />
+              <p style="font-size:0.875rem;line-height:1.25rem;margin:16px 0;font-weight:700;color:rgb(43,144,142)">AbegHelp.me</p>
+            </div>
+            <p style="font-size:1rem;line-height:1.5rem;margin:16px 0;text-align:center">Your journey into fundraising with ease</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>`
+}
