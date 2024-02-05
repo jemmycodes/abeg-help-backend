b79fb26deef6d8653d8382b5eabe5272aa9e36db
Bayode Emmanuel <obcbeats@gmail.com>
2024-02-05 22:22:16 +0300

    Get-campaigns (#151)
    * fix
    
    * update

--- src/queues/templates/welcomeEmail.ts
+++ src/queues/templates/welcomeEmail.ts
@@ -1,6 +1,5 @@
 export const welcomeEmail = (data) => {
-	return;
-	`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 		<meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
 		<html dir="ltr" lang="en">
 		
@@ -87,6 +86,5 @@ export const welcomeEmail = (data) => {
 			  </tbody>
 			</table>
 		  </body>
-		
 		</html>`;
 };
