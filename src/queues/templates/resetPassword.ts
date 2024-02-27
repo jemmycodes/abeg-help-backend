d800b86046d0f08ad7c1eab0640b73fa650d76ce
Otebele Jemimah (Chef Meemah) <110843645+jemmycodes@users.noreply.github.com>
2024-02-27 08:19:28 +0000

    fix(email-template):Reset Password Mail

--- src/queues/templates/resetPassword.ts
+++ src/queues/templates/resetPassword.ts
@@ -1,69 +1,132 @@
 export const resetPassword = () => {
 	return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-		<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-			<head>
-				<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-			</head>
-			<body style="background-color: #fff; font-family: Arial, sans-serif;">
-				<table
-					width="100%"
-					cellspacing="0"
-					cellpadding="0"
-					border="0"
-					style="max-width: 600px; margin: 0 auto; padding: 20px;"
-				>
-					<tr>
-						<td>
-							<h1 style="color: #000; font-size: 24px; text-align: center; margin: 0;">
-								Your password has been changed as you requested.
-							</h1>
-							<p style="font-size: 14px; line-height: 24px; margin: 16px 0;">
-								If you didn't request to change your password, we're here to help keep your account secure. Visit our
-								<a href="test@test.com" target="_blank" style="color: #2563eb; text-decoration: none;">support page</a>
-								for more info.
-							</p>
-							<table
-								width="100%"
-								border="0"
-								cellpadding="0"
-								cellspacing="0"
-								role="presentation"
-								style="text-align: center;"
-							>
-								<tr>
-									<td>
-										<a
-											href="https://abeg-help.com/contact"
-											target="_blank"
-											style="line-height: 100%; text-decoration: none; display: inline-block; padding: 12px 20px; background-color: #000; border-radius: 4px; color: #fff; font-size: 12px; font-weight: 600; text-align: center;"
-											>Contact us</a
-										>
-									</td>
-								</tr>
-							</table>
-							<hr style="width: 100%; border: 1px solid #eaeaea; margin: 26px 0;" />
-							<table
-								width="100%"
-								border="0"
-								cellpadding="0"
-								cellspacing="0"
-								role="presentation"
-								style="text-align: center;"
-							>
-								<tr>
-									<td>
-										<p style="font-size: 14px; line-height: 24px; margin: 16px 0;"><strong>abeg-help</strong></p>
-									</td>
-								</tr>
-							</table>
-							<p style="font-size: 12px; line-height: 24px; margin: 16px 0; color: #666;">
-								This message was sent to
-								<a href="test@test.com" target="_blank" style="color: #2563eb; text-decoration: none;">test@test.com</a
-								>. If you have questions or complaints, please contact us.
-							</p>
-						</td>
-					</tr>
-				</table>
-			</body>
-		</html> `;
+<html dir="ltr" lang="en">
+
+  <head>
+    <meta content="text/html; charset=UTF-8" http-equiv="Content-Type" />
+    <title>Reset your AbegHelp! Password</title>
+  </head>
+
+  <body>
+    <table align="center" width="100%" class="" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="max-width:24rem;width:100%;color:rgb(72,72,72);margin-left:auto;margin-right:auto">
+      <tbody>
+        <tr style="width:100%">
+          <td>
+            <style>
+              @font-face {
+                font-family: 'Manrope';
+                font-style: normal;
+                font-weight: 400;
+                mso-font-alt: 'sans-serif';
+                src: url(https://fonts.gstatic.com/s/manrope/v15/xn7gYHE41ni1AdIRggexSg.woff2) format('woff2');
+              }
+
+              * {
+                font-family: 'Manrope', sans-serif;
+              }
+            </style>
+            <div style="display:none;overflow:hidden;line-height:1px;opacity:0;max-height:0;max-width:0">AbegHelp - Reset Password!</div>
+            </div>
+            <table align="center" width="100%" class="" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="width:100%;max-width:100px;margin-left:auto;margin-right:auto">
+              <tbody>
+                <tr>
+                  <td>
+                    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+                      <tbody style="width:100%">
+                        <tr style="width:100%">
+                          <td data-id="__react-email-column"><img alt="Abeg Help Logo" height="30" src="https://res.cloudinary.com/dsg6otcag/image/upload/v1704816639/abeghelp%20assets/abeg_logo_nfbolw.png" style="display:block;outline:none;border:none;text-decoration:none;margin-right:0.5rem" width="30" /></td>
+                          <td data-id="__react-email-column">
+                            <p style="font-size:1.125rem;line-height:1.75rem;margin:16px 0;font-weight:700;color:rgb(43,144,142)">AbegHelp.me</p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <h1 style="font-size:1.25rem;line-height:1.75rem;letter-spacing:0.025em;font-weight:700;text-align:center">Reset your AbegHelp Password!</h1>
+            <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+              <tbody>
+                <tr>
+                  <td><img alt="Abeg help Illustration" src="https://res.cloudinary.com/dsg6otcag/image/upload/v1704811249/abeghelp%20assets/6538623_1_rqlcxj.png" style="display:block;outline:none;border:none;text-decoration:none;max-width:24rem;height:13rem;width:100%;object-fit:cover" /></td>
+                </tr>
+              </tbody>
+            </table>
+            <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+              <tbody>
+                <tr>
+                  <td>
+                    <p class="" style="font-size:1.25rem;line-height:1.75rem;margin:16px 0">Hi, Ola</p>
+                    <p style="font-size:14px;line-height:24px;margin:16px 0">To ensure the security of your account and complete the password reset process, please use the password reset link below;</p><a style="color:rgb(255,255,255);text-decoration:none;background-color:rgb(43,144,142);padding-left:1.75rem;padding-right:1.75rem;padding-top:0.5rem;padding-bottom:0.5rem;margin-top:0.75rem;margin-bottom:0.75rem;border-radius:0.375rem;text-align:center;margin-left:auto;margin-right:auto;cursor:pointer" target="_blank">Reset Password</a>
+                    <p style="font-size:14px;line-height:24px;margin:16px 0">This verification link will expire in 24 hours for security</p>
+                    <p style="font-size:14px;line-height:24px;margin:16px 0">If you didn&#x27;t request this password reset, please disregard this email</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+              <tbody>
+                <tr>
+                  <td>
+                    <p style="font-size:14px;line-height:24px;margin:16px 0">Best Regards,<br />The team</p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+              <tbody>
+                <tr>
+                  <td>
+                    <p style="font-size:0.875rem;line-height:1.25rem;margin:16px 0">This email was sent to<span style="color:rgb(43,144,142);font-weight:500;text-decoration-line:underline">thelocsdesigner@gmail.com</span>because you signed up for an account on AbegHelp.me
+                    <p style="font-size:14px;line-height:24px;margin:16px 0">© 2024 Abeghelp.me</p>
+                    </p>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table align="center" width="100%" class="" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="width:100%;max-width:100px;margin-left:auto;margin-right:auto">
+              <tbody>
+                <tr>
+                  <td>
+                    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+                      <tbody style="width:100%">
+                        <tr style="width:100%">
+                          <td data-id="__react-email-column"><img alt="Abeg Help Logo" height="20" src="https://res.cloudinary.com/dsg6otcag/image/upload/v1704811240/abeghelp%20assets/fb_icon_ysucrx.png" style="display:block;outline:none;border:none;text-decoration:none;margin-right:0.5rem" width="20" /></td>
+                          <td data-id="__react-email-column"><img alt="Abeg Help Logo" height="20" src="https://res.cloudinary.com/dsg6otcag/image/upload/v1704811241/abeghelp%20assets/linkdin_icn_wpfzka.png" style="display:block;outline:none;border:none;text-decoration:none;margin-right:0.5rem" width="20" /></td>
+                          <td data-id="__react-email-column"><img alt="Abeg Help Logo" height="20" src="https://res.cloudinary.com/dsg6otcag/image/upload/v1704811240/abeghelp%20assets/tw_icon_m3kszi.png" style="display:block;outline:none;border:none;text-decoration:none;margin-right:0.5rem" width="20" /></td>
+                          <td data-id="__react-email-column"><img alt="Abeg Help Logo" height="20" src="https://res.cloudinary.com/dsg6otcag/image/upload/v1704811240/abeghelp%20assets/insta_icn_r1nqj2.png" style="display:block;outline:none;border:none;text-decoration:none;margin-right:0.5rem" width="20" /></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <table align="center" width="100%" class="" border="0" cellPadding="0" cellSpacing="0" role="presentation" style="width:100%;max-width:100px;margin-left:auto;margin-right:auto">
+              <tbody>
+                <tr>
+                  <td>
+                    <table align="center" width="100%" border="0" cellPadding="0" cellSpacing="0" role="presentation">
+                      <tbody style="width:100%">
+                        <tr style="width:100%">
+                          <td data-id="__react-email-column"><img alt="Abeg Help Logo" height="15" src="https://res.cloudinary.com/dsg6otcag/image/upload/v1704816639/abeghelp%20assets/abeg_logo_nfbolw.png" style="display:block;outline:none;border:none;text-decoration:none;margin-right:0.5rem" width="15" /></td>
+                          <td data-id="__react-email-column">
+                            <p style="font-size:0.75rem;line-height:1rem;margin:16px 0;font-weight:700;color:rgb(43,144,142)">AbegHelp.me</p>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <p style="font-size:14px;line-height:24px;margin:16px 0;text-align:center">Your journey into fundraising with ease</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </body>
+
+</html>`;
 };
